11a30ad36300295f434543c70658285afedddd8b
avhz <RustQuantContact@gmail.com>
2024-02-06 08:28:34 +1100

    Initial Commit

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// calenda-rs: A Rust library for global calendars.
+// Copyright (C) 2024 https://github.com/avhz
+//
+// Dual licensed under Apache 2.0 and MIT.
+//
+// See:
+//      - LICENSE-APACHE.md
+//      - LICENSE-MIT.md
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+pub mod calendar;
+pub mod holiday;
